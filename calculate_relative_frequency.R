2c4d3e91176fe5f82bbc7a82fa896a34ee9b10b9
Maxime Rivest <Maxime Rivest>
2016-05-17 17:50:52 -0400

    Corrected column names of the frequencies tables and set order to order it by frequency first rather than relative frequencies first

--- calculate_relative_frequency.R
+++ calculate_relative_frequency.R
@@ -12,10 +12,11 @@ calculate_relative_frequency <- function(freq_df) {
     sum_tot <- sum(new_df$Frequency)
     sum_sub <- sum(new_df$i.Frequency)
     new_df$rel_freq <- (new_df$i.Frequency/sum_sub)/(new_df$Frequency/sum_tot)
-    setorder(new_df, -rel_freq,-i.Frequency)
-    freq_df[[i]] <- as.data.frame(new_df)
+    setorder(new_df, -i.Frequency, -rel_freq)
+    new_df <- as.data.frame(new_df)
+    names(new_df)[3:4] <-  c("Frequency", "Relative Frequency")
+    freq_df[[i]] <- new_df
     freq_df[[i]] <- freq_df[[i]][,-2]
-    names(freq_df[[i]][,c(2,3)]) <- c("Frequency", "Relative Frequency")
   }
   return(freq_df)
 }
