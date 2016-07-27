10f0d175bfd1f3abb3a843229fa5fd5e350ad192
Maxime Rivest <mrive052@gmail.com>
2016-07-26 23:28:09 -0400

    copied scimeetr import function

--- from_WOS_files_to_WOS_table.R
+++ from_WOS_files_to_WOS_table.R
@@ -1,34 +1,39 @@
 #------------------------------------------------------------------------------
 # To work on only one WOS file. Merge and import all WOS files in the directory
 #------------------------------------------------------------------------------
-from_WOS_files_to_WOS_table <- function(files_directory) {
-  WOS_file       <- NULL
-  folder_content <- list.files(files_directory)
-  files_quantity <- length(folder_content)
-
-  for (files in 1:files_quantity) {
-    full_file_path <- paste(files_directory,folder_content[files],sep = "")
-    if (files == 1) {
-      WOS_table <- read.delim2(full_file_path, header = T,
-                               fileEncoding = "UTF-8",
-                               row.names = NULL,
-                               quote = "",
-                               stringsAsFactors = FALSE,
-                               comment.char = "")
-    } else {
-      WOS_table_temp <- read.delim2(full_file_path, header = T,
-                               fileEncoding = "UTF-8",
-                               row.names = NULL,
-                               quote = "",
-                               stringsAsFactors = FALSE,
-                               comment.char = "")
-      WOS_table <- rbind(WOS_table, WOS_table_temp)
+from_WOS_files_to_WOS_table <- function (files_directory) 
+{
+    WOS_file <- NULL
+    folder_content <- list.files(files_directory)
+    files_quantity <- length(folder_content)
+    for (files in 1:files_quantity) {
+        full_file_path <- paste(files_directory, folder_content[files], 
+            sep = "")
+        if (files == 1) {
+            fileName <- full_file_path
+            v_char <- readChar(full_file_path, file.info(full_file_path)$size)
+            v_char <- iconv(v_char, from = "UTF-8", to = "ASCII", 
+                sub = "")
+            writeChar(v_char, con = full_file_path, eos = NULL)
+            WOS_table <- read.delim2(full_file_path, header = T, 
+                fileEncoding = "ASCII", row.names = NULL, quote = "", 
+                stringsAsFactors = FALSE, comment.char = "")
+        }
+        else {
+            fileName <- full_file_path
+            v_char <- readChar(full_file_path, file.info(full_file_path)$size)
+            v_char <- iconv(v_char, from = "UTF-8", to = "ASCII", 
+                sub = "")
+            writeChar(v_char, con = full_file_path, eos = NULL)
+            WOS_table_temp <- read.delim2(full_file_path, header = T, 
+                fileEncoding = "ASCII", row.names = NULL, quote = "", 
+                stringsAsFactors = FALSE, comment.char = "")
+            WOS_table <- rbind(WOS_table, WOS_table_temp)
+        }
     }
-  }
-  # Fix misplaced column names
-  column_names <- names(WOS_table)[2:length(names(WOS_table))]
-  WOS_table <- WOS_table[, 1:(ncol(WOS_table) - 1)]
-  names(WOS_table) <- column_names
-  WOS_table <- WOS_table[!duplicated(WOS_table), ]
-  return(WOS_table)
+    column_names <- names(WOS_table)[2:length(names(WOS_table))]
+    WOS_table <- WOS_table[, 1:(ncol(WOS_table) - 1)]
+    names(WOS_table) <- column_names
+    WOS_table <- WOS_table[!duplicated(WOS_table), ]
+    return(WOS_table)
 }
